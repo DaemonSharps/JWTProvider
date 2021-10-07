72fb9ffe312ad2cab76d9a050003fada8b1a078e
Денис Смирнов <smirnov.god@gmail.com>
2021-10-08 01:21:31 +0300

    Перенес сваггер в прод версию

--- src/JWTProvider/Startup.cs
+++ src/JWTProvider/Startup.cs
@@ -50,9 +50,14 @@ namespace JWTProvider
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTProvider v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTProvider v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
