24ba0a1e4f06856d5dcc18453c0fb0298ec868f8
DaemonSharps <smirnov.god@gmail.com>
2022-06-19 22:18:10 +0300

    Добавил в инфраструктуру возможность обработки исключений слоя

---
+++ src/Infrastructure/Common/Exceptions/LayerException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common.Exceptions
+{
+    public abstract class LayerException : Exception
+    {
+        public LayerException(HttpStatusCode statusCode, string errorCode, string errorMessage)
+            : base(errorMessage)
+        {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+
+        public virtual string ContentType => @"text/plain";
+
+        public virtual string Layer { get; } = "JWTProvider";
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        private string ErrorCode { get; set; }
+
+        public ApiError Error 
+        { 
+            get 
+            {
+                return new()
+                {
+                    ErrorCode = ErrorCode,
+                    ErrorMessage = Message
+                };
+            }
+        }
+    }
+
+    public class ApiError
+    {
+        public string ErrorCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
