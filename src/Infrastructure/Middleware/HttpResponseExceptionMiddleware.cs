770c98f8f48484ea890979210841cf03f2d729f4
DaemonSharps <smirnov.god@gmail.com>
2022-06-22 00:10:11 +0300

    переработал LayerException на HttpResponseException Добавил возможность добавлять внутреннее исключение и логгировать его

--- src/Infrastructure/Middleware/HttpResponseExceptionMiddleware.cs
+++ src/Infrastructure/Middleware/HttpResponseExceptionMiddleware.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
-using Infrastructure.Common.Exceptions;
+﻿using Infrastructure.Common.Exceptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Middleware
 {
