fbc1fef319a1c7eb24760e5e9efc55aa5fb13285
DaemonSharps <smirnov.god@gmail.com>
2022-06-24 01:53:35 +0300

    вернул JWTClaimKeys

---
+++ src/Infrastructure/Constants/JWTClaimKeys.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Constants
+{
+    public struct JWTClaimKeys
+    {
+        public const string Role = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+
+        public const string Login = "login";
+
+        public const string MiddleName = "middle_name";
+
+        public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+    }
+}
