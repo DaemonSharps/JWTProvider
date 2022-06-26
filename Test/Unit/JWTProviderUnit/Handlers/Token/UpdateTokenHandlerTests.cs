c532e5a35249cbd08d06ce79d97aa7e6d8c71231
DaemonSharps <smirnov.god@gmail.com>
2022-06-26 15:41:46 +0300

    Разметил будующие unit тесты

---
+++ Test/Unit/JWTProviderUnit/Handlers/Token/UpdateTokenHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handlers.Token
+{
+    public class UpdateTokenHandlerTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.False(true);
+        }
+    }
+}
