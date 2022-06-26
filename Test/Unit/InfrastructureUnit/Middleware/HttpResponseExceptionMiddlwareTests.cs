22e4660645def202d6088e2758ff392513f22c2d
DaemonSharps <smirnov.god@gmail.com>
2022-06-26 15:41:46 +0300

    Разметил будующие unit тесты

---
+++ Test/Unit/InfrastructureUnit/Middleware/HttpResponseExceptionMiddlwareTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class HttpResponseExceptionMiddlwareTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.False(true);
+        }
+    }
+}
