f5d6c49f2645ff27c404ce900fb898e24c079d48
DaemonSharps <smirnov.god@gmail.com>
2022-06-27 20:52:55 +0300

    тесты GetTokenHandler

---
+++ Test/Unit/JWTProviderUnit/Mocks/TestDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mocks;
+
+internal static class TestDBContext
+{
+    public static UsersDBContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<UsersDBContext>()
+                      .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                      .Options;
+        return new UsersDBContext(options);
+    }
+}
+
