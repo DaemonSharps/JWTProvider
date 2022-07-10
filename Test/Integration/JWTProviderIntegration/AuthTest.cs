046a56a9ab9a22e0132c0d9610300d2a866d99ce
DaemonSharps <smirnov.god@gmail.com>
2022-07-11 00:09:40 +0300

    перенес классы для апи тестов

--- Test/Integration/JWTProviderIntegration/AuthTest.cs
+++ Test/Integration/JWTProviderIntegration/AuthTest.cs
@@ -1,63 +1,39 @@
 ﻿using JWTProvider;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Infrastructure.DataBase;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Infrastructure.Middleware;
 using System.Net.Http.Json;
 using JWTProvider.User.Commands;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using JWTProvider.Token.Commands;
+using JWTProviderIntegration.Common;
 
 namespace Token;
 
-public class TestFixture : WebApplicationFactory<Startup>
+public class AuthTest : ApiTestBase
 {
-    protected override IHostBuilder CreateHostBuilder()
+    public AuthTest(JWTProviderTestFixture<Startup> fixture) : base(fixture)
     {
-        var builder = Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webHostBuilder =>
-            {
-                webHostBuilder.UseStartup<Startup>();
-                webHostBuilder.ConfigureTestServices(c =>
-                {
-                    var descr = c.Single(s => s.ServiceType == typeof(DbContextOptions<UsersDBContext>));
-                    c.Remove(descr);
-                    c.AddDbContext<UsersDBContext>(o => o.UseInMemoryDatabase(Guid.NewGuid().ToString()));
-                });
-            })
-            .ConfigureAppConfiguration((cont, conf)
-                => conf.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: false));
-        return builder;
-    }
-
-}
-
-public class AuthTest : IClassFixture<TestFixture>
-{
-    private readonly HttpClient _client;
-
-    public AuthTest(TestFixture fixture)
-    {
-        _client = fixture.CreateClient();
     }
 
     [Fact]
     public async Task Test()
     {
-        var content = JsonContent.Create(new UserRegistrationCommand
+        var regCommand = new UserRegistrationCommand
         {
             Email = "tes@mail.ru",
             Password = "test",
             FirstName = "1"
-        });
-       var result = await _client.PostAsync("/user", content);  
-       result.EnsureSuccessStatusCode();
-       Assert.NotNull(result);
+        };
+        var result = await Client.PostAsJsonAsync("/user", regCommand);
+        result.EnsureSuccessStatusCode();
+        Assert.NotNull(result);
+
+        var getToken = new GetTokenCommand
+        {
+            Email = regCommand.Email,
+            Password = regCommand.Password
+        };
+
+        var res = await Client.PostAsJsonAsync("/token", getToken);
+        res.EnsureSuccessStatusCode();
+        Assert.NotNull(res);
     }
 }
 
---
+++ Test/Integration/JWTProviderIntegration/Common/JWTProviderTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.DataBase;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JWTProviderIntegration.Common
+{
+    public class JWTProviderTestFixture<TStartup> : WebApplicationFactory<TStartup>
+        where TStartup : class
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            var builder = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                {
+                    webHostBuilder.UseStartup<TStartup>();
+                })
+                .ConfigureAppConfiguration((cont, conf)
+                    => conf.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: false));
+            return builder;
+        }
+    }
+}
+
