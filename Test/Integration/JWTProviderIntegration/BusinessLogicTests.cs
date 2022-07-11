f3cbc4326960c84f3ebb86c3b5f8f197e0d65911
DaemonSharps <smirnov.god@gmail.com>
2022-07-11 20:21:04 +0300

    интеграционные тесты бизнес логики

---
+++ Test/Integration/JWTProviderIntegration/BusinessLogicTests.cs
@@ -0,0 +1,103 @@
+﻿using JWTProvider;
+using JWTProviderIntegration.Common;
+using Infrastructure.Entities;
+using JWTProviderIntegration.Extentions;
+
+namespace BusinessLogicTests;
+
+public class BusinessLogicTests : ApiTestBase
+{
+    public BusinessLogicTests(JWTProviderTestFixture<Startup> fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task RegistrationForNewUser()
+    {
+        var expectedUser = new User
+        {
+            Email = "t@mail.ru",
+            FirstName = "fn",
+            LastName = "ln",
+            MiddleName = "mn"
+        };
+        const string ExpectedPassword = "t";
+        var registrationResult = await Client.UserRegistration(expectedUser, ExpectedPassword);
+
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        var updateTokenResult = await Client.CheckRefreshToken(expectedUser, registrationResult.RefreshToken);
+        Assert.NotEqual(registrationResult.AccessToken, updateTokenResult.AccessToken);
+    }
+
+    [Fact]
+    public async Task LoginExistingUser()
+    {
+        var expectedUser = new User
+        {
+            Email = "test@mail.ru",
+            FirstName = "Денис",
+            MiddleName = "Смирнов",
+            LastName = "Алексеевич"
+        };
+
+        const string ExpectedPassword = "test";
+        var loginResult = await Client.GetToken(expectedUser, ExpectedPassword);
+
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        var updateTokenResult = await Client.CheckRefreshToken(expectedUser, loginResult.RefreshToken);
+        Assert.NotEqual(loginResult.AccessToken, updateTokenResult.AccessToken);
+    }
+
+
+    [Fact]
+    public async Task RegistrationForNewUserAndUpdate()
+    {
+        var expectedUser = new User
+        {
+            Email = "t2@mail.ru",
+            FirstName = "fn",
+            LastName = "ln",
+            MiddleName = "mn"
+        };
+        const string ExpectedPassword = "t";
+        var registrationResult = await Client.UserRegistration(expectedUser, ExpectedPassword);
+
+        var updateTokenResult = await Client.CheckRefreshToken(expectedUser, registrationResult.RefreshToken);
+
+        expectedUser.LastName = "different_ln";
+        expectedUser.MiddleName = "different_mn";
+        expectedUser.FirstName = "different_fn";
+        await Client.UpdateUser(expectedUser, updateTokenResult.AccessToken);
+
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        var updateTokenResultAfterUserUpdate = await Client.CheckRefreshToken(expectedUser, updateTokenResult.RefreshToken);
+        Assert.NotEqual(updateTokenResult.AccessToken, updateTokenResultAfterUserUpdate.AccessToken);
+    }
+
+    [Fact]
+    public async Task LoginExistingUserAndUpdate()
+    {
+        var expectedUser = new User
+        {
+            Email = "test@mail.ru",
+            FirstName = "Денис",
+            MiddleName = "Смирнов",
+            LastName = "Алексеевич"
+        };
+
+        const string ExpectedPassword = "test";
+        var loginResult = await Client.GetToken(expectedUser, ExpectedPassword);
+
+        var updateTokenResult = await Client.CheckRefreshToken(expectedUser, loginResult.RefreshToken);
+
+        expectedUser.LastName = "different_ln";
+        expectedUser.MiddleName = "different_mn";
+        expectedUser.FirstName = "different_fn";
+        await Client.UpdateUser(expectedUser, updateTokenResult.AccessToken);
+
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        var updateTokenResultAfterUserUpdate = await Client.CheckRefreshToken(expectedUser, updateTokenResult.RefreshToken);
+        Assert.NotEqual(updateTokenResult.AccessToken, updateTokenResultAfterUserUpdate.AccessToken);
+    }
+}
+
---
+++ Test/Integration/JWTProviderIntegration/Extentions/HttpClientExtentions.Token.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Infrastructure.Entities;
+using JWTProvider.Models;
+using JWTProvider.Token.Commands;
+using JWTProvider.User.Commands;
+using JWTProviderIntegration.Common;
+using JwtTest.Common;
+
+namespace JWTProviderIntegration.Extentions
+{
+    internal static partial class HttpClientExtentions
+    {
+        public static async Task<TokenModel> GetToken(this HttpClient client, User user, string password)
+        {
+            var command = new GetTokenCommand
+            {
+                Email = user.Email,
+                Password = password
+            };
+
+            var result = await client.PostAsJsonAsync("/token", command);
+            result.EnsureSuccessStatusCode();
+
+            var tokenModel = await result.Content.ReadFromJsonAsync<TokenModel>();
+
+            Assert.NotNull(tokenModel);
+            var jsonWebToken = JWTAssert.IsJWT(tokenModel.AccessToken, TestTokenOptions.AccessKey, TestTokenOptions.Issuer);
+            JWTAssert.IsValidHeader(jsonWebToken);
+            JWTAssert.IsValidPayload(jsonWebToken, user, TestTokenOptions.Issuer);
+            Assert.NotNull(tokenModel.RefreshToken.ToString());
+
+            return tokenModel;
+        }
+
+        public static async Task<TokenModel> CheckRefreshToken(this HttpClient client, User user, Guid refreshToken)
+        {
+            var command = new UpdateTokenCommand
+            {
+                RefreshToken = refreshToken
+            };
+
+            var result = await client.PutAsJsonAsync("/token", command);
+            result.EnsureSuccessStatusCode();
+
+            var tokenModel = await result.Content.ReadFromJsonAsync<TokenModel>();
+
+            Assert.NotNull(tokenModel);
+            Assert.NotEqual(refreshToken, tokenModel.RefreshToken);
+            var jsonWebToken = JWTAssert.IsJWT(tokenModel.AccessToken, TestTokenOptions.AccessKey, TestTokenOptions.Issuer);
+            JWTAssert.IsValidHeader(jsonWebToken);
+            JWTAssert.IsValidPayload(jsonWebToken, user, TestTokenOptions.Issuer);
+            Assert.NotNull(tokenModel.RefreshToken.ToString());
+
+            return tokenModel;
+        }
+    }
+}
+
