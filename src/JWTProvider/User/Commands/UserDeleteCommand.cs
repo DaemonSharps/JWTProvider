78cb276a9251fc567c313803b895ea0503f51538
DaemonSharps <smirnov.god@gmail.com>
2022-11-02 01:54:55 +0300

    обработчик закрытия учетной записи пользователя

---
+++ src/JWTProvider/User/Commands/UserDeleteCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace JWTProvider.User.Commands;
+
+public class UserDeleteCommand : IRequest<Infrastructure.DataBase.Entities.User>
+{
+    public string Email { get; set; }
+}
+
