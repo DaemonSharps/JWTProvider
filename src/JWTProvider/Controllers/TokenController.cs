c4c046811017e3e79531becd0c06da9d5d154fe0
DaemonSharps <smirnov.god@gmail.com>
2022-11-01 17:46:56 +0300

    использование обновления сессии в контроллере токенов

--- src/JWTProvider/Controllers/TokenController.cs
+++ src/JWTProvider/Controllers/TokenController.cs
@@ -1,10 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Infrastructure.Common;
 using Infrastructure.Common.Exceptions;
 using Infrastructure.CustomAttributes.Swagger;
+using Infrastructure.DataBase.Entities;
+using Infrastructure.Middleware.Options;
 using JWTProvider.Models;
+using JWTProvider.Session.Commands;
 using JWTProvider.Token.Commands;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace JWTProvider.Controllers
@@ -26,9 +32,32 @@ namespace JWTProvider.Controllers
         [SwaggerOperation("Check the token and get a new pair of JWT and RT")]
         [SwaggerResponse(200, "Token verified", typeof(TokenModel))]
         [SwaggerResponse(400, "An error was occured", typeof(ApiError))]
-        public async Task<IActionResult> CheckRefreshToken(UpdateTokenCommand command)
+        public async Task<IActionResult> CheckRefreshToken(UpdateSessionCommand command, [FromServices] IOptions<TokenOptions> options)
         {
-            var model = await Mediator.Send(command);
+            string accessToken = null;
+            Guid? refreshToken = null;
+            try
+            {
+                var session = await Mediator.Send(command);
+
+                refreshToken = session.RefreshToken;
+                accessToken = JWTGenerator
+                    .GetGenerator(options.Value)
+                    .CreateAcessToken(session.User)
+                    .AcessToken;
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+
+            //TODO: избавиться от использования UpdateTokenHandler
+            var tokenModel = await Mediator.Send(new UpdateTokenCommand { RefreshToken = command.RefreshToken });
+            var model = new TokenModel
+            {
+                RefreshToken = tokenModel.RefreshToken,
+                AccessToken = accessToken ?? tokenModel.AccessToken
+            };
             return Ok(model);
         }
     }
